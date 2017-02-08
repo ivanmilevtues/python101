43694bc35c1455d02122fd8b2206f23324a80078
ivanmilevtues <ivanmilevtues@gmail.com>
2017-02-08 16:15:47 +0200

    Adding statistics

--- week14/hackFMI/statistics.py
+++ week14/hackFMI/statistics.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -5,7 +6,7 @@ from prettytable import PrettyTable
 
 from model import SkillList, PublicTeam, MentorList
 from start import Base
-from getInfo import mentors, public_teams, skills
+# from getInfo import mentors, public_teams, skills
 
 engine = create_engine("sqlite:///hackaton.db")
 Base.metadata.create_all(engine)
@@ -57,9 +58,49 @@ def take_team_stats(name):
     print(table)
 
 
+def take_all_mentors():
+    all_mentors = session.query(MentorList).all()
+    mentor_list = set()
+    for mentor in all_mentors:
+        mentor_list.add(mentor.name)
+    return mentor_list
+
+
+def take_all_teams():
+    all_teams = session.query(PublicTeam).all()
+    team_list = set()
+    for team in all_teams:
+        team_list.add(team.name)
+    return team_list
+
+
+def convert_time(hour, mins):
+    if mins > 60:
+        hour += mins / 60
+        mins = mins % 60
+    if mins < 10:
+        return str(hour) + ':' + str(mins)
+    else:
+        return str(hour) + ':0' + str(mins)
+
+
+def manage_hack(teams):
+    mins = 0
+    hour = 19
+    agenta = []
+    for team in teams:
+        start = convert_time(hour, mins)
+        mins += 15
+        end = convert_time(hour, mins)
+        agenta.append((team, start, end))
+    return agenta
+
+
 def main():
-    take_mentor_stats("Явор Стойчев")
-    take_team_stats("2b|!2b")
+    # take_mentor_stats("Явор Стойчев")
+    # take_team_stats("2b|!2b")
+    teams = take_all_teams()
+    pprint(manage_hack(teams))
 
 
 if __name__ == '__main__':
